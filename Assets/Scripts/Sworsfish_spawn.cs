dd7e15dd60c4da91e8b3e0ef105c3e2f623208b3
Felix <felix1.johans1@gmail.com>
2026-03-03 12:47:38 +0100

    Sender inn filene om en annen enemy type som er sword fish som mister H har lagd

---
+++ Assets/Scripts/Sworsfish_spawn.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public float spawnYMin = -4f;
+    public float spawnYMax = 4f;
+    public float spawnX = -20f;  // Left side spawn
+    public float spawnRate = 1f;
+
+    void Start()
+    {
+        InvokeRepeating(nameof(SpawnEnemy), 1f, spawnRate);
+    }
+
+    void SpawnEnemy()
+    {
+        float randomY = Random.Range(spawnYMin, spawnYMax);
+        Vector3 spawnPos = new Vector3(spawnX, randomY, 0f);
+
+        Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
+    }
+}
